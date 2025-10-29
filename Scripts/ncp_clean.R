1dee99cc91eb9d102d7296471dd25203a87717d1
Flavien Petit <flavien.petit@obs-vlfr.fr>
2025-10-29 13:43:47 +0000

    First results

---
+++ Scripts/ncp_clean.R
@@ -0,0 +1,299 @@
+library(tidyverse)
+library(lubridate)
+library(castr)
+library(zoo)
+
+dat <- read_csv("Data/Processed/float_nitrate_data_corrected.csv")
+
+
+# filter the dataset to stay with ICB -------------------------------------
+#add iceland contour on map
+
+ggplot(select(dat, lon, lat) |> distinct())+
+  geom_point(aes(x = lon, y = lat))+
+  geom_rect(aes(xmin = -45, xmax = -10, ymin = 54, ymax = 63), fill = NA, color = "red")+
+  theme_minimal()+#add land contour
+  borders("world", xlim = c(-60, 0), ylim = c(50, 70), fill = "lightgrey", color = "black")+
+  coord_quickmap(xlim = c(-55, - 10), ylim = c(50, 65))
+
+ggsave("Output/float_locations_icb.png", width = 8, height = 6)
+dat <- filter(dat, lon >= -45 & lon <= -10 & lat >= 54 & lat <= 63)
+range(dat$date)
+#filter to keep data between 2019 and now
+dat <- filter(dat, date >= as.Date("2022-01-01"))
+# MLD and Zeu time series -------------------------------------------------
+
+#Artificially set zeu as 40 if NA
+# dat <- dat |> 
+#   mutate(zeu = case_when(is.na(zeu) ~ 40,
+#                          TRUE ~ zeu))
+#Create a dataset with MLD and Zeu for each profile 
+dat_prof <- dat |> select(float_wmo, prof_number, date, MLD, zeu) |> 
+  distinct() |> 
+  arrange(date) |> 
+  filter(!is.na(MLD) & !is.na(zeu))
+
+#Smooth the MLD and Zeu time series using a spline
+# Remove NAs before smoothing
+dat_smooth <- dat_prof %>%
+  filter(!is.na(date), !is.na(MLD), !is.na(zeu))
+
+# Fit spline
+fit_mld <- smooth.spline(as.numeric(dat_smooth$date), dat_smooth$MLD, spar = 0.6)
+fit_zeu <- smooth.spline(as.numeric(dat_smooth$date), dat_smooth$zeu, spar = 0.6)
+# Predict smoothed values for all dates
+pred_mld <- predict(fit_mld, as.numeric(dat_prof$date))$y
+pred_zeu <- predict(fit_zeu, as.numeric(dat_prof$date))$y
+
+
+# Add back to main data frame
+dat_prof <- dat_prof %>%
+  mutate(mld_smooth = pred_mld,
+         zeu_smooth = pred_zeu)
+
+
+ggplot(dat_prof)+
+  geom_line(aes(x = date, y = -mld_smooth, color = "MLD"))+
+  geom_point(aes(x = date, y = -mld_smooth), shape = 1)+
+  geom_line(aes(x = date, y = -zeu_smooth, color = "Zeu"))+
+  labs(y = "Depth (m)", color = "Parameter")+
+  theme_minimal()
+ggsave("Output/mld_zeu_time_series_icb.png", width = 10, height = 6)
+  
+dat <- left_join(dat, select(dat_prof, prof_number, float_wmo, mld_smooth, zeu_smooth), by = c("float_wmo", "prof_number"))
+
+
+# Integration depth computation -------------------------------------------
+
+
+#Define Integration depth (i.e. deepest value of MLD or Zeu between t and t-1)
+integration_depth_data <- dat |>
+  group_by(float_wmo, prof_number, date) |>
+  summarise(
+    mld_smooth = unique(mld_smooth, na.rm = TRUE),
+    zeu_smooth = unique(zeu_smooth, na.rm = TRUE)) |> 
+  ungroup()
+
+#Regrid the data on a 10day interval timeseries
+time_grid <- tibble(date_10day = seq(min(integration_depth_data$date),
+                                     max(integration_depth_data$date),
+                                     by = "15 days"))
+
+prof_dat_smoothed <- integration_depth_data %>%
+  mutate(date_10day = as.Date(cut(date, breaks = "15 days"))) %>%
+  group_by(date_10day) %>%
+  summarise(mld = mean(mld_smooth, na.rm = TRUE),
+            zeu = mean(zeu_smooth, na.rm = TRUE)) |> 
+  mutate(
+    prev_MLD = dplyr::lag(mld),
+    prev_zeu = dplyr::lag(zeu),
+    NCP_integration_depth = pmax(mld, prev_MLD, zeu, prev_zeu, na.rm = TRUE),
+    next_integration_depth = lead(NCP_integration_depth)
+  ) |> 
+  full_join(time_grid, by = "date_10day") |>
+  arrange(date_10day) %>%
+  mutate(
+    NCP_integration_depth = zoo::na.approx(NCP_integration_depth, x = as.numeric(date_10day), na.rm = FALSE, rule = 2),
+    next_integration_depth = zoo::na.approx(next_integration_depth, x = as.numeric(date_10day), na.rm = FALSE, rule = 2)
+  ) %>%
+  filter(date_10day %in% time_grid$date_10day) |> 
+  mutate(dt = as.numeric(date_10day - dplyr::lag(date_10day)),
+         NCP_integration_depth_smooth = rollapply(NCP_integration_depth, width = 3, FUN = mean, align = "center", fill = NA),
+         next_integration_depth_smooth = rollapply(next_integration_depth, width = 3, FUN = mean, align = "center", fill = NA)
+  )
+
+ggplot(filter(prof_dat_smoothed, !is.na(mld)))+
+  geom_line(aes(x = date_10day, y = -NCP_integration_depth_smooth))+
+  geom_line(aes(x = date_10day, y = -next_integration_depth_smooth), linetype = "dashed")
+
+ggsave("Output/integration_depth_time_series_icb.png", width = 10, height = 6)
+prof_dat_smoothed <- na.omit(prof_dat_smoothed)
+
+
+# Interpolation of data  --------------------------------------------------
+
+#Create a dataset with the median value of nitrate integrals every 10 days for smoothing
+dat_smoothed <- dat %>%
+  mutate(date_10day = as.Date(cut(date, breaks = "15 days"))) %>%
+  group_by(date_10day, depth) %>%
+  summarise(
+    nitrate = mean(nitrate_corrected, na.rm = TRUE),
+    npp = mean(cbpm_npp, na.rm = TRUE)) |> 
+  ungroup() |> 
+  full_join(time_grid, by = "date_10day") %>%     # ensure regular 10-day spacing
+  arrange(date_10day) %>%
+  group_by(depth) |> 
+  mutate(
+    nitrate = na.approx(nitrate, x = as.numeric(date_10day), na.rm = FALSE, rule = 2),
+    npp = na.approx(npp, x = as.numeric(date_10day), na.rm = FALSE, rule = 2)) %>%
+  ungroup() |> 
+  filter(date_10day %in% time_grid$date_10day)
+
+ggplot(dat_smoothed)+
+  geom_point(aes(x = nitrate, y = - depth, color = date_10day))
+ggsave("Output/nitrate_profiles_icb.png", width = 8, height = 6)
+dat_smoothed <- dat_smoothed |> 
+  left_join(prof_dat_smoothed, by = "date_10day") |> 
+  na.omit()
+
+
+# Integration of nitrate over integration depth ---------------------------
+
+dat_final <- dat_smoothed |>
+  group_by(date_10day, mld, zeu) |>
+  filter(!is.na(next_integration_depth)) |> 
+  summarise(
+    int_N_mmol_m2 = integrate(nitrate, depth, from = 0, to = unique(NCP_integration_depth)),
+    next_int_N_mmol_m2 = integrate(nitrate, depth, from = 0, to = unique(next_integration_depth)),
+    npp = integrate(npp, depth, from = 0, to = 200),
+    mld_concentration = integrate(nitrate, depth, from = unique(NCP_integration_depth) - 20, to = unique(NCP_integration_depth) - 10) / 10,
+    sub_mld_concentration = integrate(nitrate, depth, from = unique(NCP_integration_depth) + 20, to = unique(NCP_integration_depth) + 30) / 10
+  ) |> 
+  ungroup()
+
+ggplot(dat_final)+
+  geom_line(aes(x = date_10day, y = int_N_mmol_m2))+
+  geom_point(aes(x = date_10day, y = int_N_mmol_m2))+
+  geom_line(aes(x = date_10day, y = next_int_N_mmol_m2), linetype = "dashed")+
+  labs(y = "Integrated Nitrate (mmol N m-2)")+
+  theme_minimal()+
+  scale_x_date(date_labels = "%b %Y", breaks = "1 months")
+
+ggsave("Output/integrated_nitrate_time_series_icb.png", width = 10, height = 6)
+#Smoothing the integration
+dat_final_smoothed <- dat_final %>%
+  mutate(
+    int_N_smooth = rollapply(int_N_mmol_m2, width = 3, FUN = mean, align = "center", fill = NA),
+    next_int_N_smooth = rollapply(next_int_N_mmol_m2, width = 3, FUN = mean, align = "center", fill = NA)
+  )
+
+ggplot(dat_final_smoothed)+
+  geom_line(aes(x = date_10day, y = int_N_mmol_m2), linetype = "dashed")+
+  geom_line(aes(x = date_10day, y = int_N_smooth))+
+  labs(y = "Integrated Nitrate (mmol N m-2)")+
+  theme_minimal()+
+  scale_x_date(date_labels = "%b %Y", breaks = "2 months")+
+  ggtitle("Smoothed Integrated Nitrate Time Series ICB")
+
+ggsave("Output/integrated_nitrate_smooth_time_series_icb.png", width = 10, height = 6)
+# Computing NCP -----------------------------------------------------------
+
+
+ncp_results <- dat_final_smoothed |> 
+  arrange(date_10day) |> 
+  mutate(dt = as.numeric(date_10day - dplyr::lag(date_10day)),
+         diff_mld = mld - lag(mld),
+         we = pmax(0, diff_mld),
+         delta = sub_mld_concentration - mld_concentration,
+         diff = delta * we,
+         nitrate_consumption =  dplyr::lag(next_int_N_smooth) - int_N_smooth,
+         c_consumption = nitrate_consumption * 6.625 / dt,
+         NCP = c_consumption + diff) |> 
+  ungroup()
+
+ncp_results <- ncp_results |> 
+  mutate(ncp_smooth = rollapply(NCP, width = 6, FUN = mean, align = "center", fill = NA),
+         ncp_sd = rollapply(NCP, width = 6, FUN = sd, align = "center", fill = NA),
+         npp_smooth = rollapply(npp, width = 6, FUN = median, align = "center", fill = NA),
+         npp_sd = rollapply(npp, width = 6, FUN = sd, align = "center", fill = NA))
+
+ggplot(ncp_results)+
+  geom_line(aes(x = date_10day, y = NCP))+
+  geom_line(aes(x = date_10day, y = npp_smooth/12))+
+  geom_point(aes(x = date_10day, y = NCP, color = we))+
+  scale_color_viridis_c()
+
+
+ggplot(ncp_results)+
+  geom_line(aes(x = date_10day, y = npp_smooth/12, color = "NPP"))+
+  geom_errorbar(aes(x = date_10day, y = npp_smooth/12, ymin = npp_smooth/12 - npp_sd/12, ymax = npp_smooth/12 + npp_sd/12), alpha = 0.3)+
+  geom_errorbar(aes(x = date_10day, ymin = ncp_smooth - ncp_sd, ymax = ncp_smooth + ncp_sd), alpha = 0.3)+
+  geom_line(aes(x = date_10day, y = ncp_smooth, color = "NCP"))+
+  labs(y = "mmol C m-2 d-1")+
+  theme_bw()+
+  scale_x_date(date_labels = "%b %Y", breaks = "1 months")+
+  xlab("Date")
+
+ggsave("Output/ncp_time_series_icb.png", width = 10, height = 6)
+
+ncp_results <- ncp_results |> 
+  mutate(status = case_when(ncp_smooth > 0 ~ "autotrophic",
+                            ncp_smooth < 0 ~ "heterotrophic",
+                            TRUE ~ "balanced"))
+
+ggplot(ncp_results)+
+  geom_point(aes(x = date_10day, y = ncp_smooth, color = status))+
+  geom_line(aes(x = date_10day, y = npp_smooth/12), linetype = "dashed")+
+  labs(y = "mmol C m-2 d-1")+
+  scale_color_manual(values = c("autotrophic" = "#a6d854", "heterotrophic" = "#fb8072"))+
+  theme_minimal()+
+  scale_x_date(date_labels = "%b %Y", breaks = "2 months")+
+  xlab("Date")
+
+ggsave("Output/ncp_status_time_series_icb.png", width = 10, height = 6)
+
+ncp_results <- ncp_results |>
+  mutate(trophic_index = ncp_smooth/npp_smooth)
+
+ggplot(ncp_results)+
+  geom_line(aes(x = date_10day, y = trophic_index))+
+  geom_point(aes(x = date_10day, y = trophic_index, color = status))+
+  scale_color_manual(values = c("autotrophic" = "blue", "heterotrophic" = "red", "balanced" = "grey"))+
+  theme_minimal()+
+  scale_x_date(date_labels = "%b %Y", breaks = "2 months")+
+  xlab("Date")+
+  ylab("Trophic Index (NCP/NPP)")
+
+ggplot(ncp_results)+
+  geom_line(aes(x = date_10day, y = -mld))+
+  geom_point(aes(x = date_10day, y = -mld, color = diff))+
+  scale_color_viridis_c()
+
+ggplot(ncp_results)+
+  geom_point(aes(x = date_10day, y = - mld, color = NCP))+
+  scale_color_distiller(palette = "RdBu")+
+  theme_dark()
+
+# draft -------------------------------------------------------------------
+
+# # Ensure data are ordered by profile and date
+# dat <- dat |> arrange(prof_number, date, depth)
+# 
+# # Step 1: Compute integrated nitrate down to each possible depth limit
+# integrate_to_depth <- function(df, depth_limit) {
+#   sub <- df |> filter(depth <= depth_limit)
+#   if (nrow(sub) < 2) return(NA)  # Need at least 2 points for integration
+#   trapz(sub$depth, sub$nitrate)
+# }
+# 
+# # Step 2: Compute integrated nitrate to 200 m for sanity check
+# dat_integrated_200 <- dat |>
+#   group_by(prof_number, date, MLD) |>
+#   summarise(int_N_200_mmol_m2 = integrate_to_depth(cur_data(), 200), .groups = "drop")
+# 
+# # Step 3: Compute NCP integration depth dynamically
+# dat_integrated <- dat_integrated_200 |>
+#   arrange(date) |>
+#   mutate(
+#     prev_MLD = lag(MLD),
+#     NCP_integration_depth = pmax(MLD, prev_MLD, 40, na.rm = TRUE),
+#     next_integration_depth = lead(NCP_integration_depth)
+#   )
+# 
+# dat_joined <- left_join(dat, dat_integrated)
+# 
+# dat_final <- dat_joined |>
+#   group_by(prof_number, date, MLD, NCP_integration_depth, next_integration_depth) |>
+#   summarise(
+#     int_N_mmol_m2 = integrate_to_depth(pick(nitrate, depth), unique(NCP_integration_depth)),
+#     next_int_N_mmol_m2 = integrate_to_depth(pick(nitrate, depth), unique(next_integration_depth)),
+#     .groups = "drop"
+#   )
+# 
+# dat_final <- dat_final |> 
+#   mutate(nitrate_consumption = int_N_mmol_m2 - lag(next_int_N_mmol_m2),
+#          c_consumption = nitrate_consumption * 6.625 / 10,
+#          NCP = - c_consumption * 12)  # Convert to mmol C m-2
+# 
+# ggplot(filter(dat_final, NCP > -4000))+
+#   geom_line(aes(x = date, y = NCP))
---
+++ Scripts/sprof_to_csv.R
@@ -0,0 +1,161 @@
+library(ncdf4)
+library(dplyr)
+library(tidyr)
+library(lubridate)
+library(zoo)
+
+# open the Sprof NetCDF
+#nc <- nc_open("Data/Raw/Floats/3901586_Sprof.nc")
+
+files <- list.files("Data/Raw/Floats/", pattern = "_Sprof.nc", full.names = TRUE)
+for(file in files){
+  print(paste(file, "started..."))
+  nc <- nc_open(file)
+  # extract metadata
+  lon   <- ncvar_get(nc, "LONGITUDE")
+  lat   <- ncvar_get(nc, "LATITUDE")
+  juld  <- ncvar_get(nc, "JULD")  # days since 1950-01-01 00:00:00
+  date  <- as.Date(juld, origin = "1950-01-01")
+  
+  # extract depth and variables (dimensions usually [levels x profiles])
+  depth <- ncvar_get(nc, "PRES")  
+  chla  <- ncvar_get(nc, "CHLA_ADJUSTED")
+  bbp   <- ncvar_get(nc, "BBP700_ADJUSTED")   # sometimes "BBP700_ADJUSTED"
+  #ipar  <- ncvar_get(nc, "DOWNWELLING_PAR")
+  temp  <- ncvar_get(nc, "TEMP")
+  sal   <- ncvar_get(nc, "PSAL")
+  nitrate <- ncvar_get(nc, "NITRATE")
+  oxygen <- ncvar_get(nc, "DOXY_ADJUSTED")
+  
+  #create filename
+  wmo <- ncvar_get(nc, "PLATFORM_NUMBER")[1]
+  #remove whitespace
+  wmo <- gsub(" ", "", wmo)
+  filename <- paste0("Data/Intermediate/Floats/argo_", wmo, "_interp.csv")
+  
+  # close connection
+  nc_close(nc)
+  
+  # get dimensions
+  n_levels   <- dim(depth)[1]
+  n_profiles <- dim(depth)[2]
+  
+  # repeat metadata across depth levels
+  lon_rep  <- rep(lon, each = n_levels)
+  lat_rep  <- rep(lat, each = n_levels)
+  date_rep <- rep(date, each = n_levels)
+  
+  # flatten arrays into vectors
+  df <- tibble(
+    lon   = lon_rep,
+    lat   = lat_rep,
+    date  = date_rep,
+    depth = as.vector(depth),
+    chla  = as.vector(chla),
+    bbp   = as.vector(bbp),
+    #iPAR  = as.vector(ipar),
+    temp  = as.vector(temp),
+    sal   = as.vector(sal),
+    nitrate = as.vector(nitrate),
+    oxygen = as.vector(oxygen)
+  )
+  
+  df_nona <- filter(df, !is.na(temp))
+  df_nona <- df_nona |> mutate(depth = round(depth))
+  
+  df_interp <- df_nona %>%
+    group_by(lon, lat, date) %>%
+    arrange(depth, .by_group = TRUE) %>%
+    reframe(
+      depth = seq(0,
+                  2000, 
+                  by = 1)
+    ) %>%
+    left_join(df_nona, by = c("lon", "lat", "date", "depth")) %>%
+    arrange(lon, lat, date, depth) %>%
+    group_by(lon, lat, date) %>%
+    mutate(across(
+      c(chla, bbp, oxygen, temp, sal, nitrate),
+      ~ if (sum(!is.na(.x)) > 10) {
+        na.approx(.x, x = depth, na.rm = FALSE, rule = 2)
+      } else {
+        rep(NA_real_, length(.x))  # if no interpolation possible
+      }
+    )) %>%
+    ungroup() |> 
+    group_by(lon, lat, date, depth) |> 
+    summarise_all(mean, na.rm = TRUE) |> 
+    ungroup()
+  
+  df_interp <- df_interp %>%
+    arrange(date) %>% 
+    mutate(prof_number = dense_rank(date))
+  
+  t <- filter(df_interp, prof_number == 75)
+  
+  # save the csv ------------------------------------------------------------
+
+  write_csv(df_interp, filename)
+  print(paste0(filename, " written"))
+}
+
+
+# Plotting raw data -------------------------------------------------------
+
+# # create folders
+# dir.create("output/plots/temp_sal", recursive = TRUE, showWarnings = FALSE)
+# dir.create("output/plots/chla_bbp", recursive = TRUE, showWarnings = FALSE)
+# 
+# for (p in unique(df_interp$prof_number)) {
+#   
+#   df_prof <- df_interp %>% filter(prof_number == p)
+#   profile_date <- unique(df_prof$date)
+#   
+#   ## ---- Plot 1: Temperature + Salinity ----
+#   scale_factor_ts <- max(df_prof$temp, na.rm = TRUE) / 
+#     max(df_prof$sal,  na.rm = TRUE)
+#   
+#   p1 <- ggplot(df_prof, aes(y = depth)) +
+#     geom_path(aes(x = temp, colour = "Temperature")) +
+#     geom_path(aes(x = sal * scale_factor_ts, colour = "Salinity")) +
+#     scale_y_reverse(expand = c(0,0)) +
+#     scale_x_continuous(
+#       name = "Temperature (°C)",
+#       sec.axis = sec_axis(~ . / scale_factor_ts, name = "Salinity (PSU)")
+#     ) +
+#     labs(
+#       title = paste("Profile", p, "-", profile_date),
+#       y = "Depth (m)",
+#       colour = ""
+#     ) +
+#     theme_minimal()
+#   
+#   ## ---- Plot 2: Chla + BBP (dual x axis, top 100 m) ----
+#   df_prof100 <- df_prof %>% filter(depth <= 300)
+#   
+#   # scaling factor between Chla and BBP ranges
+#   scale_factor <- max(df_prof100$chla, na.rm = TRUE) / 
+#     max(df_prof100$bbp,  na.rm = TRUE)
+#   
+#   p2 <- ggplot(df_prof100, aes(y = depth)) +
+#     geom_path(aes(x = chla, colour = "Chla")) +
+#     geom_path(aes(x = bbp * scale_factor, colour = "BBP")) +
+#     scale_y_reverse(expand = c(0,0)) +
+#     scale_x_continuous(
+#       name = "Chla (mg/m³)",
+#       sec.axis = sec_axis(~ . / scale_factor, name = "BBP (1/m)")
+#     ) +
+#     labs(
+#       title = paste("Profile", p, "-", profile_date, "(0–300 m)"),
+#       y = "Depth (m)",
+#       colour = ""
+#     ) +
+#     theme_minimal()
+#   
+#   ggsave(
+#     filename = file.path("output/plots/chla_bbp", paste0("profile_", p, "_chla_bbp.png")),
+#     plot = p2,
+#     width = 6, height = 8
+#   )
+# }
+
